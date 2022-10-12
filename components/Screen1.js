aca625f18d8f5485b0a6ac9747ade70c58887c46
Martin Guido <guidomartin7@gmail.com>
2022-10-12 19:24:07 -0300

    added parameters

--- components/Screen1.js
+++ components/Screen1.js
@@ -1,12 +1,18 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput} from 'react-native';
+import React, {useState} from 'react';
 
 function Screen1({navigation}) {
+  const [nombre, setNombre] = useState('');
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Pantalla 1</Text>
+      <TextInput
+        placeholder="Ingrese su nombre"
+        onChangeText={text1 => setNombre(text1)}
+      />
       <Button
         title="Ir a Pantalla 2"
-        onPress={() => navigation.navigate('Screen2')}
+        onPress={() => navigation.navigate('Screen2', {text: nombre})}
       />
     </View>
   );
--- components/Screen2.js
+++ components/Screen2.js
@@ -1,9 +1,11 @@
 import {View, Text, Button} from 'react-native';
 
 function Screen2({route, navigation}) {
+  const {text} = route.params;
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Pantalla 2</Text>
+      <Text>Bienvenido {text}</Text>
+
       <Button title="Volver a pantalla 1" onPress={() => navigation.goBack()} />
     </View>
   );
