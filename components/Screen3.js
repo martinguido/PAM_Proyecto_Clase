6991915d809af288ca56eae22aabe371cf28ed47
Martin Guido <guidomartin7@gmail.com>
2022-10-12 21:13:31 -0300

    final commit

--- components/Screen3.js
+++ components/Screen3.js
@@ -1,7 +1,15 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-function Screen1({}) {
+function Screen3({navigation}) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setCount(count + 1);
+    });
+    console.log(count);
+  });
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Pantalla 3</Text>
@@ -9,4 +17,4 @@ function Screen1({}) {
   );
 }
 
-export default Screen1;
+export default Screen3;
